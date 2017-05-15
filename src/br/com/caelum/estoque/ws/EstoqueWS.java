911ab364250e6551f47e1a79947f3290c58086c9
Adriano Melo <adriano.de.melo@accenture.com>
2017-05-15 11:55:41 -0300

    Alterado wsdl e soap mais expressivo ao cliente

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,6 +3,7 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
@@ -19,7 +20,7 @@ public class EstoqueWS {
 	
 	@WebMethod(operationName="todosOsItens")
 	@WebResult(name="itens")
-	public ListaItens getItens(Filtros filtros) {
+	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
 		System.out.println("Chamando todosItens()");
 		List<Filtro> lista = filtros.getLista();
 		List<Item> itensResultado = dao.todosItens(lista);
