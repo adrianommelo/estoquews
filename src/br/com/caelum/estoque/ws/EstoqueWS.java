00238d743503e449e4d5a0b9814c9fb815a6f93a
Adriano Melo <adriano.de.melo@accenture.com>
2017-05-16 09:42:43 -0300

    Adicinado Cadastrar item e Fault na mensagem

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,27 +6,39 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
-import br.com.caelum.estoque.modelo.item.Filtro;
-import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
-import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
 public class EstoqueWS {
 
 	ItemDao dao = new ItemDao();
-	
+
 	@WebMethod(operationName="todosOsItens")
-	@WebResult(name="itens")
-	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
-		System.out.println("Chamando todosItens()");
-		List<Filtro> lista = filtros.getLista();
-		List<Item> itensResultado = dao.todosItens(lista);
-		//criando uma lista de itens
-		return new ListaItens(itensResultado);
+	@ResponseWrapper(localName="itens")
+	@WebResult(name="item")
+	@RequestWrapper(localName="listaItens")
+	public List<Item> getItens() {
+		System.out.println("Chamando getItens()");
+	    return dao.todosItens();
 	}
 	
+	@WebMethod(operationName="CadastrarItem")
+	@WebResult(name="item")
+	public Item cadastrarItem(
+			@WebParam(name="tokenUsuario", header=true) TokenUsuario tokenUsuario,  
+			@WebParam(name="item") Item item) throws AutorizacaoException {
+		if(!new TokenDao().ehValido(tokenUsuario)){
+			throw new AutorizacaoException("Autorizacao falhou.");
+		}
+		System.out.println("Cadastrando " + item + " token: " +tokenUsuario);
+		this.dao.cadastrar(item);
+		return item;
+	}
+
 }
- 
